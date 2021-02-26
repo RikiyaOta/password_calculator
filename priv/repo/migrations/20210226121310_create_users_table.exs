65a83194576cab89b1db60575c00c557d381e0ea
rikiyaota <rikiya0421.fcb6@gmail.com>
2021-02-26 21:18:08 +0900

    Add a new migration for creating users table.

---
+++ priv/repo/migrations/20210226121310_create_users_table.exs
@@ -0,0 +1,25 @@
+defmodule PasswordCalculator.Repo.Migrations.CreateUsersTable do
+  use Ecto.Migration
+
+  def up do
+    execute("""
+      CREATE TABLE public.users (
+        id uuid NOT NULL,
+        email varchar(256) NOT NULL,
+        password varchar(256) NOT NULL,
+        name varchar(128) NOT NULL,
+        phone_number varchar(32) NOT NULL,
+        base_key varchar(512) NOT NULL,
+        inserted_at timestamp with time zone NOT NULL,
+        updated_at timestamp with time zone NOT NULL,
+
+        CONSTRAINT pk_users PRIMARY KEY (id),
+        CONSTRAINT uq_email UNIQUE (email)
+      );
+    """)
+  end
+
+  def down do
+    execute("DROP TABLE public.users;")
+  end
+end
