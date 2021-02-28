9f1a2879bc9f404756fc0d361df0373847ebf8cd
rikiyaota <rikiya0421.fcb6@gmail.com>
2021-02-28 17:11:58 +0900

    Add a database backup script.

---
+++ script/backup_database.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# $1 = Mix Env [required]
+
+set -e
+
+if [ -z "$1" ] || [ "$1" = "" ]; then
+  echo "Pass Mix Env as the First Argument."
+  exit 1
+fi
+
+if [ "$1" != "prod" ] && [ "$1" != "dev" ]; then
+  echo "Invalid Mix Env."
+  exit 1
+fi
+
+MIX_ENV=$1
+
+BASE_DIR=$(dirname $0)
+cd $BASE_DIR
+
+PGPASSFILE="./auth/$MIX_ENV/database/$MIX_ENV.pgpass"
+HOST=$(cat ./hosts/$MIX_ENV/database)
+USERNAME=$(cat ./auth/$MIX_ENV/database/username)
+PORT="5432"
+DBNAME="password_calculator_db"
+
+TIMESTAMP=$(date '+%Y%m%d%H%M%S')
+OUTPUT_FILE="./backup/${DBNAME}__${MIX_ENV}__${TIMESTAMP}.sql"
+
+PGPASSFILE=$PGPASSFILE pg_dump \
+  --dbname=$DBNAME \
+  --host=$HOST \
+  --port=$PORT \
+  --username=$USERNAME \
+  --file=$OUTPUT_FILE
